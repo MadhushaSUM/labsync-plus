0b0ad8fad735438e4e215f9b03745b10a6117e56
Madhusha Laksitha <madhusha.sum@gmail.com>
2023-09-07 11:46:21 +0530

    Create Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
 
-import Image from 'next/image';
-import { signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-
 export default function Home() {
-
-  const { data:session } = useSession();
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='text-4xl font-bold'>LabSync Plus</h1>
-      <h3><span>Welcome </span>{session?.user?.username}</h3>
-      <button onClick={() => signOut()}>Sign Out</button>
     </main>
   )
 }
