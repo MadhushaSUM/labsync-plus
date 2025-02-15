7f1677be68f1c15db9422a94d6ddef0adc065614
Madhusha Laksitha <madhusha.sum@gmail.com>
2025-02-15 17:37:57 +0530

    delete session call when logging out

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,8 +1,13 @@
 "use server";
 
-import { signOut } from "@/auth";
+import { signOut, auth } from "@/auth";
+import { deleteSessionByUserId } from "@/data/user";
 
 export const logout = async () => {
+    const session = await auth();
+    await deleteSessionByUserId(session?.user.id as number | undefined);
+    console.log(session);
+
     await signOut({
         redirectTo: "/",
     });
--- data/user.ts
+++ data/user.ts
@@ -15,3 +15,17 @@ export async function fetchUserById(id?: string) {
     const res = await response.json()
     return res.user as UserType;
 }
+
+export async function deleteSessionByUserId(userId?: number) {
+    if (!userId) {
+        throw new Error("user id must be defined!");
+    }
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/logout?userId=${userId}`);
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to delete session!");
+    }
+
+    return await response.json()
+}
