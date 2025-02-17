b09aea9600dacc16f2ca0de9c72e69c9f009451e
Madhusha Laksitha <madhusha.sum@gmail.com>
2025-02-17 10:34:38 +0530

    initial login session bug fix

--- hooks/api/auth/useCurrentUser.ts
+++ hooks/api/auth/useCurrentUser.ts
@@ -4,5 +4,9 @@ import { useSession } from "next-auth/react"
 export const useCurrentUser = () => {
     const session = useSession();
 
+    if (session.status != 'authenticated') {
+        window.location.reload();
+    }
+
     return session.data?.user;
 }
